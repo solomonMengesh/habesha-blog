035c987138cc764eae99fe67366d422e747ac529
Solomon <smengesha838@gmail.com>
2025-03-03 08:59:44 -0900

    Refactor application structure by updating routing, adding protected routes, and enhancing component organization

--- src/index.js
+++ src/index.js
@@ -1,19 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
-// reportWebVitals is optional for performance tracking, remove it if you don't need it
-// import reportWebVitals from './reportWebVitals';
 
-const rootElement = document.getElementById('root');
-
-if (rootElement) {
-  ReactDOM.createRoot(rootElement).render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
-}
-
-// If you want performance reporting, leave this in. Otherwise, you can comment it out.
-// reportWebVitals();
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
