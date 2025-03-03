05bdcaa92efd6c75e7b531ff7bd8501063c9af61
Solomon <smengesha838@gmail.com>
2025-03-03 08:38:53 -0900

    Refactor context structure by moving AuthContext and ThemeContext to a new directory

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { ThemeProvider } from './context/ThemeContext';
-import { AuthProvider } from './context/AuthContext';
+import { ThemeProvider } from './contexts/ThemeContext';
+import { AuthProvider } from './contexts/AuthContext';
 
 // Pages
 import HomePage from './pages/Home';
